285439152509418bc258913c850242dd7714ef41
Adamnoise <csarli713@gmail.com>
2025-04-20 00:49:31 +0200

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -3,6 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // We'll use environment variables later for the URL and anon key
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL || '', 
-  import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+  import.meta.env.VITE_SUPABASE_URL || 'https://zajsujrzfuuyjbmdabca.supabase.co', 
+  import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InphanN1anJ6ZnV1eWpibWRhYmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxMDIzNjgsImV4cCI6MjA2MDY3ODM2OH0.g_bUvQV-HKqtO49ZQnjgvNy0Ar2CdTw6zknFA3esF0k'
 );
